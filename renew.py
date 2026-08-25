c67d524750fc8589d389cedb640470f1df524ed5
zq163126 <zq163126@outlook.com>
2026-08-25 15:31:36 +0800

    Update renew.py

--- renew.py
+++ renew.py
@@ -142,8 +142,8 @@ def run_auto_renew():
         "accept": "application/x-tss-framed, application/x-ndjson, application/json",
         "authorization": f"Bearer {token}",
         "content-type": "application/json",
-        "origin": "https://new.freemchost.com",
-        "referer": f"https://new.freemchost.com/app/servers/{SERVER_ID}",
+        "origin": "https://freemchost.com",
+        "referer": f"https://freemchost.com/app/servers/{SERVER_ID}",
         "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/148.0.0.0 Safari/537.36",
         "x-tsr-serverfn": "true"
     }
